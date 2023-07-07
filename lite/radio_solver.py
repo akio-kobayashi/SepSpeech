fix mixed-precision related bugs in the loss functions

lite/radio_solver.py
@@ -59,15 +59,17 @@ class LitDenoiser(pl.LightningModule):
             d[prefix+'stft_loss'] = _stft_loss1 + _stft_loss2
         if self.pesq_loss is not None:
             with torch.amp.autocast('cuda', dtype=torch.float32):
-                _pesq_loss = self.pesq_loss(targets, estimates)
+                _pesq_loss = torch.mean(self.pesq_loss(targets, estimates))
             _loss += self.pesq_loss_weight * _pesq_loss
             d[prefix+'pesq_loss'] = _pesq_loss
         if self.sdr_loss is not None:
-            _sdr_loss = self.sdr_loss(estimates, targets)
+            with torch.amp.autocast('cuda', dtype=torch.float32):
+                _sdr_loss = torch.mean(self.sdr_loss(estimates, targets))
             _loss += self.sdr_loss_weight * _sdr_loss
             d[prefix+'sdr_loss'] = _sdr_loss
         if self.stoi_loss is not None:
-            _stoi_loss = self.stoi_loss(estimates, targets)
+            with torch.amp.autocast('cuda', dtype=torch.float32):
+                _stoi_loss = torch.mean(self.stoi_loss(estimates, targets))
             _loss += self.stoi_loss_weight * _stoi_loss
             d[prefix+'stoi_loss'] = _stoi_loss
         if valid is True:

loss/stoi_loss.py
@@ -171,7 +171,7 @@ class NegSTOILoss(nn.Module):
             corr_comp = y_prim * x_seg
             # J, M as in [1], eq.6
             correction = x_seg.shape[1] * x_seg.shape[-1]
-
+            
         # Compute average (E)STOI w. or w/o VAD.
         sum_over = list(range(1, x_seg.ndim))  # Keep batch dim
         if self.use_vad:
@@ -212,7 +212,7 @@ class NegSTOILoss(nn.Module):
         # Last frame not taken because NFFT size is larger, torch bug IMO.
         x_padded = torch.nn.functional.pad(x, pad=[0, hop])
         return torch.stft(x_padded, fft_size, hop_length=hop, window=win,
-                          center=False, win_length=win_len)
+                          center=False, win_length=win_len, return_complex=False)
 
     @staticmethod
     def rowcol_norm(x, mask=None):