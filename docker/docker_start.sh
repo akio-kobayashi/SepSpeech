14dce2b90b08aecf5437abc60fe071fbdd7b181e
akio-kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-07-18 15:40:09 +0900

    fix bugs

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -2,7 +2,7 @@
 
 # uidは自分の計算機の/etc/passwdを見るなどして変更する
 uid=1000
-image=pytorch:radio
+image=pytorch:20230718
 
 docker run --rm -it -v /tmp:/tmp -v /mnt/:/mnt -v /home:/home \
      -v /media/:/media -v /etc/group:/etc/group:ro \
