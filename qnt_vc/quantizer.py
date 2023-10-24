c89b8c76aedeb46f8ae163f2620fcf052337b86e
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-10-24 09:01:48 +0900

    add files

---
+++ qnt_vc/quantizer.py
@@ -0,0 +1,28 @@
+import torch
+import torchaudio
+from lm.qnt_encode import EncodecQuantizer
+import pandas as pd
+from argparse import ArgumentParser
+import os, sys
+
+def main():
+    encoder = EncodecQuantizer
+    df = pd.read_csv(args.input_csv)
+    qnts=[]
+    for index, row in df.iterrows():
+        qnt_data = encoder.encode_from_file(row['source'])
+        path = os.path.join(args.output_root, os.path.splitext(os.path.basename(row['source']))[0] + '.pt')
+        qnts.append(path)
+        torch.save(qnt_data, path)
+
+    df['source'] = qnts
+    df.to_csv(args.output_csv)
+    
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--input_csv', type=str, required=True)
+    parser.add_argument('--output_csv', type=str, required=True)
+    parser.add_argument('--output_root', type=str, default='./')
+    args=parser.parse_args()
+
+    main(args)
