9e7be192e5abf7aca39ed1555575e1589e12c7c8
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2024-01-01 20:43:55 +0900

    fix bugs

--- metrics/evaluate_test.sh
+++ metrics/evaluate_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 -m metric.evaluate_hasqi --input_csv ../Radio/csv/ak_train_1.csv --output_csv test.csv \
+python3 -m metrics.evaluate_hasqi --input_csv ../Radio/csv/ak_train_1.csv --output_csv test.csv \
 --source_key clean --target_key noisy
 
